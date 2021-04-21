125f0d0e03ef15fde286d7907715a26aa3d13db4
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-04-21 10:16:49 -0500

    docs:Create restaurant folder

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # restaurant_go
 Backend app using go
+/home/jessica/Documents/Go/restaurant_go/Restaurants/restaurant.go
--- main.go
+++ main.go
@@ -1,23 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"Restaurants/restaurant"
+	
 )
 
 func home(writer http.ResponseWriter, request *http.Request) {
 	fmt.Print("Home")
 }
 
-func restaurants(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(writer, "index restaurants")
+// Restaurant type
+type Restaurant struct {
+	Id          int
+	Name        string
+	Description string
 }
+
+// Colection of all restaurants
+type AllRestaurants struct {
+	Restauraunts []Restaurant
+}
+
+func restaurants(writer http.ResponseWriter, request *http.Request) {
+	restaurant2 := Restaurant{1, "Restaruant name", "We love tacos"}
+	fmt.Fprint(writer, restaurant2)
+	writer.WriteHeader(http.StatusOK)
+}
+
 
 func main() {
 	fmt.Println("Server is running on port: 8080")
 	http.HandleFunc("/", home)
 
 	http.HandleFunc("/restaurants", restaurants)
+
+	restaurant1 := Restaurant{1, "Restaruant name", "We love tacos"}
+	bytesres1, _ := json.Marshal(restaurant1)
+
+
+	var restaurants AllRestaurants
+
+	if err := json.Unmarshal(bytesres1, &restaurants); err != nil {
+		fmt.Println("Error parsing json", err)
+	}
+	os.Stdout.Write(bytesres1)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
