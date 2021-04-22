8bfd8da80a47850fece86dbe4ad8cd027569f83d
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-04-22 11:47:28 -0500

    docs: Return objects from the database

--- Database/DbConnectionData/DbConnectionData.go
+++ Database/DbConnectionData/DbConnectionData.go
@@ -1,17 +1,47 @@
 package DbConnectionData
 
 import (
+	"database/sql"
 	"fmt"
 	"restaurant_go/Database/Connection"
+	comment "restaurant_go/Model/Comment.go"
 	"restaurant_go/Model/Restaurant"
-	"database/sql"
 
 	_ "github.com/lib/pq"
 )
 
-
-func NewRestaurant (name string){
+//Generate new Id for Restaurant
+func GenerateNewRestaurantId() int {
 	DbConnection.Connection();
+	db, _ := DbConnection.Open()
+
+	var newId int
+	sqlStatement := `
+	SELECT MAX(id)+1 FROM restaurants`
+	row := db.QueryRow(sqlStatement)
+	row.Scan(&newId)
+
+	db.Close()
+	return newId
+}
+
+//Generate new Id for Comment
+func GenerateNewCommentId() int {
+	DbConnection.Connection();
+	db, _ := DbConnection.Open()
+
+	var newId int
+	sqlStatement := `
+	SELECT MAX(id)+1 FROM comments`
+	row := db.QueryRow(sqlStatement)
+	row.Scan(&newId)
+
+	db.Close()
+	return newId
+}
+
+//Create new Restaurant
+func NewRestaurant (name string, description string) {
 	db, _ := DbConnection.Open()
 
 	sqlStatement := `
@@ -19,16 +49,15 @@ func NewRestaurant (name string){
 		VALUES ($1, $2, $3)
 		RETURNING id`
   id := 0
-  err := db.QueryRow(sqlStatement, 4, name, "Taquitazo feliz").Scan(&id)
+  err := db.QueryRow(sqlStatement, GenerateNewRestaurantId(), name, description).Scan(&id)
   if err != nil {
     panic(err)
   }
-  fmt.Println("New record ID is:", id)
 	db.Close();
 }
 
+//Create new Comment
 func NewComment (){
-	DbConnection.Connection();
 	db, _ := DbConnection.Open()
 
 	sqlStatement := `
@@ -46,29 +75,31 @@ func NewComment (){
 }
 
 //Restaurant show
-func GetRestaurantById(id int) {
+func GetRestaurantById(id string) restaurant.Restaurant{
+
 	db, _ := DbConnection.Open()
   sqlStatement := `SELECT * FROM restaurants WHERE id=$1;`
+
 	var restaurant restaurant.Restaurant
 	row := db.QueryRow(sqlStatement, 1)
 	err := row.Scan(&restaurant.ID, &restaurant.Nombre, &restaurant.Description)
 	switch err {
 	case sql.ErrNoRows:
 		fmt.Println("No rows were returned!")
-		return
 	case nil:
 		fmt.Println(restaurant)
 	default:
 		panic(err)
 	}
-
 	db.Close();
+	return restaurant
 }
 
 //Restaurant index
-func GetAllRestaurants() {
+func GetAllRestaurants() restaurant.AllRestaurants{
 	db, _ := DbConnection.Open()
 	rows, err := db.Query("SELECT * FROM restaurants LIMIT $1", 3)
+	var group restaurant.AllRestaurants
   if err != nil {
     panic(err)
   }
@@ -80,27 +111,30 @@ func GetAllRestaurants() {
       panic(err)
     }
     fmt.Println(restaurant)
+		group = append(group, restaurant)
   }
   err = rows.Err()
   if err != nil {
     panic(err)
   }
+
+	return group
 }
 
 func GetAllCommentsByRestaurantId(id int){
 	db, _ := DbConnection.Open()
-	rows, err := db.Query("SELECT * FROM comments WHERE restaurant_id=$1 LIMIT $2", id, 3)
+	rows, err := db.Query("SELECT * FROM comments WHERE restaurant_id=$1 LIMIT $2", id, 10)
   if err != nil {
     panic(err)
   }
   defer rows.Close()
   for rows.Next() {
-    var restaurant restaurant.Restaurant
-    err = rows.Scan(&restaurant.ID, &restaurant.Nombre, &restaurant.Description)
+    var commentario comment.Comment
+    err = rows.Scan(&commentario.Id, &commentario.Username, &commentario.Body, &commentario.RestaurantId)
     if err != nil {
       panic(err)
     }
-    fmt.Println(restaurant)
+    fmt.Println(commentario)
   }
   err = rows.Err()
   if err != nil {
--- main.go
+++ main.go
@@ -7,52 +7,50 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-	"restaurant_go/Model/Restaurant"
 	"restaurant_go/Database/Connection"
 	"restaurant_go/Database/DbConnectionData"
+	"restaurant_go/Model/Restaurant"
+
+	"github.com/gorilla/mux"
 )
 
-var restaurants = restaurant.AllRestaurants{
-	{
-		ID:          "1",
-		Nombre:      "TacoQueto",
-		Description: "Deliciosos taquitos",
-	},
-}
+var restaurants = restaurant.AllRestaurants{}
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+	fmt.Fprintf(w, "Restaurant API with Go")
 }
 
+// POST restaurant
 func createRestaurant(w http.ResponseWriter, r *http.Request) {
 	var newRestaurant restaurant.Restaurant
+
+	
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Fprintf(w, "Kindly enter data with the restaurant title and description only in order to update")
 	}
 
 	json.Unmarshal(reqBody, &newRestaurant)
-	restaurants = append(restaurants, newRestaurant)
+	DbConnectionData.NewRestaurant(newRestaurant.Nombre, newRestaurant.Description)
 	w.WriteHeader(http.StatusCreated)
-
 	json.NewEncoder(w).Encode(newRestaurant)
 }
 
+// SHOW /restaurant/1
 func getRestaurantById(w http.ResponseWriter, r *http.Request) {
 	restaurantID := mux.Vars(r)["id"]
 
-	for _, singleRestaurant := range restaurants {
-		if singleRestaurant.ID == restaurantID {
-			json.NewEncoder(w).Encode(singleRestaurant)
-		}
-	}
+	singleRestaurant := DbConnectionData.GetRestaurantById(restaurantID)
+	json.NewEncoder(w).Encode(singleRestaurant)
 }
 
+//INDEX /restaurants
 func getRestaurants(w http.ResponseWriter, r *http.Request) {
+	restaurants = DbConnectionData.GetAllRestaurants()
 	json.NewEncoder(w).Encode(restaurants)
 }
 
+// UPDATE restaurant/1 (not implemented)
 func updateRestaurantById(w http.ResponseWriter, r *http.Request) {
 	restaurantID := mux.Vars(r)["id"]
 	var updatedRestaurant restaurant.Restaurant
@@ -73,6 +71,7 @@ func updateRestaurantById(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// DELETE restaurant/2 (not implemented)
 func deleteRestaurant(w http.ResponseWriter, r *http.Request) {
 	restaurantID := mux.Vars(r)["id"]
 
@@ -92,11 +91,9 @@ func main() {
 	router.HandleFunc("/restaurant/{id}", getRestaurantById).Methods("GET")
 	router.HandleFunc("/restaurant/{id}", updateRestaurantById).Methods("PATCH")
 	router.HandleFunc("/restaurant/{id}", deleteRestaurant).Methods("DELETE")
-  connection := DbConnection.Connection()
+	connection := DbConnection.Connection()
 	fmt.Println(connection)
-	// DbConnectionData.NewRestaurant("Segundo")
-	// DbConnectionData.GetRestaurantById(1)
-	DbConnectionData.GetAllRestaurants()
+	DbConnectionData.GetAllCommentsByRestaurantId(1)
 	fmt.Println("Server is running on port: 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
